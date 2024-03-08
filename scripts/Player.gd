83d1ca9b2904b09af40c60f95cc8a4536caf7d3d
Daniel Sunthimer <d.sunthimer@gmail.com>
2024-03-09 00:05:11 +0100

    curb feelers

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -8,22 +8,31 @@ const IN_LEFT = "move_left"
 const IN_RIGHT = "move_right"
 const IN_LEAN_LEFT = "lean_left"
 const IN_LEAN_RIGHT = "lean_right"
-
 const IN_FIRE = "primary_fire"
 const IN_ALT_FIRE = "alternate_fire"
-
 const MOUSE_SENS_MOD: float = 0.005
+# ~+'^'+~ CONSTS ~+'^'+~ #
+const xz: Vector3 = Vector3(1,0,1)
+const LEDGE_HEIGHT: float = 0.55
+
 
 var mouse_sens: float = 1.0
 
 var speed: float = 5.0
 const JUMP_VELOCITY: float = 4.5
 
+# ~+'^'+~ HEURISTICS ~+'^'+~ #
+var time_since_last_jump: float = 1
+var distance_from_ground: float
+
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-@onready var cam = $PlayerCamera
-@onready var body_view = $BodyContainer/BodyViewport
-@onready var body_cam = $BodyContainer/BodyViewport/BodyCamera
+@onready var cam: Camera3D = $PlayerCamera
+@onready var body_view: SubViewport = $BodyContainer/BodyViewport
+@onready var body_cam: Camera3D = $BodyContainer/BodyViewport/BodyCamera
+@onready var pivot: Node3D = $Pivot
+@onready var curb_ray: RayCast3D = $Pivot/CurbRay
+@onready var ground_ray: RayCast3D = $GroundRay
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -33,11 +42,47 @@ func _process(_delta):
 	body_cam.global_transform = cam.global_transform
 
 func _physics_process(delta):
-	Debug.print("transform", transform)
-	Debug.print("test", mouse_sens)
-	Debug.print("cumshot", gravity)
+	Debug.print("Position", position)
 	controls(delta)
+	raycast_shit()
+	#set_floor_snap_length(delta * lerp(0, 50, time_since_last_jump))
+	#var was_on_floor: bool = is_on_floor()
+	# TODO move and slide sucks ass
 	move_and_slide()
+	#if was_on_floor:
+	#	apply_floor_snap()
+
+func raycast_shit():
+	ground_ray.force_raycast_update()
+	if ground_ray.is_colliding():
+		distance_from_ground = ground_ray.global_position.y - ground_ray.get_collision_point().y
+	else:
+		distance_from_ground = ground_ray.scale.y
+	Debug.print("Velocity", velocity)
+	Debug.print("Curb Ray Collision", "")
+	Debug.print("Ledge Detection", "")
+	Debug.print("Distance to Ground", distance_from_ground)
+	
+	# if moving, check for curb
+	if (velocity * xz).length() > 0.01 and distance_from_ground < 0.4:
+		pivot.look_at(pivot.global_position + velocity * xz)
+		curb_ray.hit_back_faces = true # because weird geometry
+		curb_ray.force_raycast_update()
+		if curb_ray.is_colliding():
+			var curb_height: float = curb_ray.get_collision_point().y - curb_ray.global_position.y + 2
+			var curb_angle: float = curb_ray.get_collision_normal().angle_to(Vector3.UP)
+			if curb_height < LEDGE_HEIGHT and curb_angle < floor_max_angle:
+				var modifier: float = (floor_max_angle - curb_angle) / floor_max_angle
+				Debug.print("Curb Ray Collision", modifier)
+				position.y += curb_height * modifier
+	# regardless, check for ledge
+	pivot.look_at(pivot.global_position - transform.basis.z)
+	curb_ray.hit_back_faces = false # because ceilings
+	curb_ray.force_raycast_update()
+	if curb_ray.is_colliding():
+		var ledge_height: float = curb_ray.get_collision_point().y - curb_ray.global_position.y + 2
+		if ledge_height > LEDGE_HEIGHT - 0.005:
+			Debug.print("Ledge Detection", ledge_height)
 
 func _input(event):
 	if cam.current and event is InputEventMouseMotion and\
@@ -53,8 +98,10 @@ func controls(delta):
 	# jump
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+	time_since_last_jump += delta
 	if Input.is_action_just_pressed(IN_JUMP) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		time_since_last_jump = 0
 	# walk
 	var input_dir = Input.get_vector(IN_LEFT, IN_RIGHT, IN_FORWARD, IN_BACKWARD)
 	var dir = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
--- scripts/global/Debug.gd
+++ scripts/global/Debug.gd
@@ -1,65 +1,34 @@
 extends Node
 class_name DebugBoy
 
-var frame: int = 0
 var info: Dictionary = {}
 var ui: Control = null
 
-func _process(_delta):
-	frame += 1
+const TEXT_HEIGHT: int = 20
+const TEXT_SIZE: int = 50
 
-# don't ask me about this
+# don't look at this if you want to continue having a good day
 func print(label_name: String, value):
 	if not info.has(label_name):
-		var label_bg = RichTextLabel.new()
-		label_bg.position = Vector2(10 + 1, info.size() / 5 * 20)
-		label_bg.size = Vector2(1,30)
-		label_bg.clip_contents = false
-		label_bg.autowrap_mode = false
-		label_bg.bbcode_enabled = true
-		info[label_name + "bg"] = label_bg
-		ui.add_child(label_bg)
-		
-		var label_bg2 = RichTextLabel.new()
-		label_bg2.position = Vector2(10 - 1, info.size() / 5 * 20)
-		label_bg2.size = Vector2(1,30)
-		label_bg2.clip_contents = false
-		label_bg2.autowrap_mode = false
-		label_bg2.bbcode_enabled = true
-		info[label_name + "bg2"] = label_bg2
-		ui.add_child(label_bg2)
-		
-		var label_bg3 = RichTextLabel.new()
-		label_bg3.position = Vector2(10, info.size() / 5 * 20 - 1)
-		label_bg3.size = Vector2(1,30)
-		label_bg3.clip_contents = false
-		label_bg3.autowrap_mode = false
-		label_bg3.bbcode_enabled = true
-		info[label_name + "bg3"] = label_bg3
-		ui.add_child(label_bg3)
-		
-		var label_bg4 = RichTextLabel.new()
-		label_bg4.position = Vector2(10, info.size() / 5 * 20 + 1)
-		label_bg4.size = Vector2(1,30)
-		label_bg4.clip_contents = false
-		label_bg4.autowrap_mode = false
-		label_bg4.bbcode_enabled = true
-		info[label_name + "bg4"] = label_bg4
-		ui.add_child(label_bg4)
-		
-		var label = RichTextLabel.new()
-		label.position = Vector2(10, info.size() / 5 * 20)
-		label.size = Vector2(1,30)
-		label.clip_contents = false
-		label.autowrap_mode = false
-		label.bbcode_enabled = true
-		info[label_name] = label
-		ui.add_child(label)
-	info[label_name + "bg"].text = "[color=black]" + label_name + " : " + str(value)
+		var neighbors: Array = [Vector2(-1,0), Vector2(1,0), Vector2(0,-1), Vector2(0,1), Vector2(0,0)]
+		for i in range(5):
+			var label: RichTextLabel = RichTextLabel.new()
+			label.position = Vector2(10, info.size() / 5 * TEXT_HEIGHT) + neighbors[i]
+			label.size = Vector2(1,30)
+			label.clip_contents = false
+			label.autowrap_mode = TextServer.AUTOWRAP_OFF
+			label.bbcode_enabled = true
+			if i < 4:
+				info[label_name + "bg" + str(i+1)] = label
+			else:
+				info[label_name] = label
+			ui.add_child(label)
+	info[label_name + "bg1"].text = "[color=black]" + label_name + " : " + str(value)
 	info[label_name + "bg2"].text = "[color=black]" + label_name + " : " + str(value)
 	info[label_name + "bg3"].text = "[color=black]" + label_name + " : " + str(value)
 	info[label_name + "bg4"].text = "[color=black]" + label_name + " : " + str(value)
-	info[label_name].text = "[color=white]" + label_name + " : " + str(value)
+	info[label_name].text = "[color=greenyellow]" + label_name + " : " + str(value)
 
 func set_ui(debug_ui):
 	self.ui = debug_ui
+	ui.set_scale(Vector2(1.3,1.3))
